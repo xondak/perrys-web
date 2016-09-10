cf1acd83e7c5963617fe23b4da3bffa97ec73e4c
xondak <gardiner.moody@gmail.com>
2016-09-10 14:36:06 -0400

    Updated to new site templating schema

--- services/send-email-form.php
+++ services/send-email-form.php
@@ -1,95 +1,97 @@
-			<?php
-				if(isset($_POST['email'])) {
+<?php
+	$LocName="Thank You";
+	$location="thank";
+    $noContent="no-content";
+	$splashContent="";
+	include '../template-parts/upper.php';
 
-				// EDIT THE 2 LINES BELOW AS REQUIRED
-				$email_to = "info@perrysfurniture.com";
-				$email_subject = "[DESIGN INQUIRY] ".$_POST['first_name']." ".$_POST['last_name'];
+	if(isset($_POST['email'])) {
 
-				function died($error) {
-				
-					// your error code can go here
-					echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-					echo "These errors appear below.<br /><br />";
-					echo $error."<br /><br />";
-					echo "Please go back and fix these errors.<br /><br />";
-					die();
-				}
+	// EDIT THE 2 LINES BELOW AS REQUIRED
+	$email_to = "info@perrysfurniture.com";
+	$email_subject = "[DESIGN INQUIRY] ".$_POST['first_name']." ".$_POST['last_name'];
 
-				// validation expected data exists
-				
-				if(!isset($_POST['first_name']) ||
-					!isset($_POST['last_name']) ||
-					!isset($_POST['email']) ||
-					!isset($_POST['telephone']) ||
-					!isset($_POST['comments'])) {
-					
-					died('We are sorry, but there appears to be a problem with the form you submitted.');       
-				}
+	function died($error) {
+	
+		// your error code can go here
+		echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+		echo "These errors appear below.<br /><br />";
+		echo $error."<br /><br />";
+		echo "Please go back and fix these errors.<br /><br />";
+		die();
+	}
 
-				$first_name = $_POST['first_name']; // required
-				$last_name = $_POST['last_name']; // required
-				$email_from = $_POST['email']; // required
-				$telephone = $_POST['telephone']; // not required
-				$comments = $_POST['comments']; // required
+	// validation expected data exists
+	
+	if(!isset($_POST['first_name']) ||
+		!isset($_POST['last_name']) ||
+		!isset($_POST['email']) ||
+		!isset($_POST['telephone']) ||
+		!isset($_POST['comments'])) {
+		
+		died('We are sorry, but there appears to be a problem with the form you submitted.');       
+	}
 
-				$error_message = "";
-				$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-				
-				if(!preg_match($email_exp,$email_from)) {
-					$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-				}
+	$first_name = $_POST['first_name']; // required
+	$last_name = $_POST['last_name']; // required
+	$email_from = $_POST['email']; // required
+	$telephone = $_POST['telephone']; // not required
+	$comments = $_POST['comments']; // required
 
-				$string_exp = "/^[A-Za-z .'-]+$/";
-				
-				if(!preg_match($string_exp,$first_name)) {
-					$error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 				}
+	$error_message = "";
+	$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+	
+	if(!preg_match($email_exp,$email_from)) {
+		$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+	}
 
-				if(!preg_match($string_exp,$last_name)) {
-					$error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-				}
-				
-				if(strlen($comments) < 2) {
-					$error_message .= 'The Comments you entered do not appear to be valid.<br />';
-				}
- 
-				if(strlen($error_message) > 0) {
-					died($error_message);
-				}
+	$string_exp = "/^[A-Za-z .'-]+$/";
+	
+	if(!preg_match($string_exp,$first_name)) {
+		$error_message .= 'The First Name you entered does not appear to be valid.<br />';
+	}
 
-				$email_message = "Form details below.\n\n";
+	if(!preg_match($string_exp,$last_name)) {
+		$error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+	}
+	
+	if(strlen($comments) < 2) {
+		$error_message .= 'The Comments you entered do not appear to be valid.<br />';
+	}
 
-				function clean_string($string) {
-					$bad = array("content-type","bcc:","to:","cc:","href");
-					return str_replace($bad,"",$string);
-				}
-				
-				$email_message .= "First Name: ".clean_string($first_name)."\n";
-				$email_message .= "Last Name: ".clean_string($last_name)."\n";
-				$email_message .= "Email: ".clean_string($email_from)."\n";
-				$email_message .= "Telephone: ".clean_string($telephone)."\n";
-				$email_message .= "Comments: ".clean_string($comments)."\n";
-				
-				// create email headers
-				$headers = 'From: '.$email_from."\r\n".
-				'Reply-To: '.$email_from."\r\n" .
-				'X-Mailer: PHP/' . phpversion();
-				@mail($email_to, $email_subject, $email_message, $headers);
-			?>
-				<link rel="text/css" href="../style.css">
-				<style type="text/css">
-					body{
-						background-color: black;
-						
-					}
-					h2, p{
-						color: white;
-					}
-				</style>
-				<img src="../res/logo.png" alt="">
-				<h2>Thank you for contacting Perry's Home Furnishings & Design.</h2>
-				</p>We will be in touch with you very soon.</p>
-				
-			<?php
-				}
-			?>
+	if(strlen($error_message) > 0) {
+		died($error_message);
+	}
+
+	$email_message = "Form details below.\n\n";
+
+	function clean_string($string) {
+		$bad = array("content-type","bcc:","to:","cc:","href");
+		return str_replace($bad,"",$string);
+	}
+	
+	$email_message .= "First Name: ".clean_string($first_name)."\n";
+	$email_message .= "Last Name: ".clean_string($last_name)."\n";
+	$email_message .= "Email: ".clean_string($email_from)."\n";
+	$email_message .= "Telephone: ".clean_string($telephone)."\n";
+	$email_message .= "Comments: ".clean_string($comments)."\n";
+	
+	// create email headers
+	$headers = 'From: '.$email_from."\r\n".
+	'Reply-To: '.$email_from."\r\n" .
+	'X-Mailer: PHP/' . phpversion();
+	@mail($email_to, $email_subject, $email_message, $headers);
+?>
+<div id="content" class="wrapper">
+	<div class="container">
+		<div class="center">
+			<img src="../res/logo-black.svg" alt="" width="500px" style="margin: 3em 0;">
+			<h2>Thank you for contacting Perry's Home Furnishings & Design.</h2>
+			<p style="margin-bottom: 3em;">We will be in touch with you very soon.</p>
+		</div>
+	</div>
+</div>
+<?php
+	}
+	include '../template-parts/lower.php';
+?>
