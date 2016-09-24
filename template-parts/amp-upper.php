0e21f9a74fd61785d5ff1541af84d8ce4e1bcc98
xondak <gardiner.moody@gmail.com>
2016-09-24 14:41:26 -0400

    Started adding amp support

---
+++ template-parts/amp-upper.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html ⚡>
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo($LocName); ?> | Perry's Home Furnishings &amp; Design</title>
+		<link rel="canonical" href="./regular-html-version.html">
+		<html lang="en-US">
+		<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+		<script async src="https://cdn.ampproject.org/v0.js"></script>
+		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+		<link rel="stylesheet" type="text/css"  href="/demo/style.css">
+		<meta name="description" content="An ever-changing assortment of fine home furnishings.">
+		<meta http-equiv="Cache-Control" content="max-age=1900"/>
+		<meta name="keywords" content="Perry&#039;s Home Furnishings &amp; Design,Furniture Store Maine,Furniture Belfast Maine,Home accessories,interior design services Maine,real estate staging services,Magnolia Home,Joanna Gaines Collection,Magnolia Home Furniture,Joanna Gaines,Chip and Joanna Gaines,Flexsteel,La-z-boy,Lazboy,Best Home Furnishings,Laura Ashley,Tempurpedic,recliners,mattresses,occasional tables,dining room table,dining room set">
+		<meta name="description" content="Maine's finest home furnishings and accessories. Come experience the difference in Belfast, Maine!">
+	</head>
+	<body>
+		<header class="site-header">
+			<?php include '../template-parts/site-header.php'; ?>
+		</header>
+        <div <?php echo('id="' . $location . '"'); ?> class="wrapper<?php echo(' ' . $noContent); ?>">
+        	<div id="bash" class="splash">
+		        <div id="splash-branding">
+                    <div class="center">
+                        <?php echo($splashContent); ?>
+                    </div>
+    	    	</div>
+        	</div>
+        </div>
