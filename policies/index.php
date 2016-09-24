409bcf0882e98bf1b15943cae66aba4f2382e0ee
xondak <gardiner.moody@gmail.com>
2016-09-24 14:41:12 -0400

    Removed junk from policies page

--- policies/index.php
+++ policies/index.php
@@ -7,112 +7,53 @@
 
 <div id="faq" class="container">
     <div class="center">
-    <h1>Policies</h1>
+    <h1>Policies and Info</h1>
         <div class="column-unbreakable">
-            <h2>General FAQs and Policies</h2>
-            <h3>1. What are your store hours?</h3>
-            <p>Monday &#8211; Friday 10:00AM-5:30PM<br />
-            Saturday 10:00AM-5:00PM<br />
-            Sunday Closed<br />
-            Or call to make an appointment.</p>
-            <h3>2. What are the sales staff&#8217;s favorite brands?</h3>
-            <p>At Perry’s Home Furnishings &#038; Design, we only carry quality furniture because we believe the investment you make in furniture should last you a lifetime. Our staff is proud of all the lines we carry, including Four Seasons, La-Z-Boy, Flexsteel, Best Home Furnishings, and Pure Latex Bliss.</p>
-            <p>Four Seasons offers quality construction and a huge variety of slip cover options, making Four Seasons “the chameleon of furniture.”</p>
-            <p>La-Z-Boy and Flexsteel are known for their quality construction and durability. They also offer huge selections of beautiful, custom upholstery choices.</p>
-            <p>If it is a funky chair at a great price that you are looking for, our staff highly recommends Best Home Furnishings.</p>
-            <p>Pure Latex Bliss is the favorite choice for quality mattresses. They are made in the USA from renewable, all natural latex. We love Pure Latex Bliss for its low environmental impact and its long lasting, high quality.</p>
-            <h3>3. Do you offer services for businesses or interior designers?</h3>
+            <h2>General Information</h2>
+            <h3>1. Do you offer services for businesses or interior designers?</h3>
             <p>We offer home staging services for real estate agents, individuals, independent designers, and anyone interested in selling a real estate property. If you have a property for sale and you want to make the best impression on potential buyers, home staging services offer a tremendous advantage in today’s competitive real estate market. Call us today to set up a consultation at (207) 338-3610. And, of course, if you are an interior designer, feel free to come in to see how we can help you serve your clients better.</p>
-            <h3>4. What if I have questions about a product?</h3>
-            <p>Call our friendly and knowledgeable staff at the store anytime during open hours at (207) 338-3610.</p>
-            <h3>5. What if I have questions about my order?</h3>
-            <p>Call our friendly and knowledgeable staff at the store anytime during open hours at (207) 338-3610.</p>
-            <h3>6. I have a question that was not answered here, how can I get the answer?</h3>
-            <p>Call our friendly and knowledgeable staff at the store anytime during open hours at (207) 338-3610.<br />
-        </div></p>
+        </div>
 
         <div class="column-unbreakable">
-            <h2>Payments FAQs and Policies</h2>
+            <h2>Payments Policies</h2>
             <h3>1. What type of payments do you accept?</h3>
-            <p>Cash, checks, VISA, MasterCard, Discover, American Express, layaway, and GE financing.</p>
+            <p>We accept the usual stuff; cash, checks, VISA, MasterCard, Discover, American Express, layaway, and Syncrony financing.</p>
             <h3>2. What kind of financing do you have?</h3>
-            <p>We offer 6 month financing with no interest through Synchrony Bank. Other options are available throughout the year. Call today for details and to see if you qualify. (207) 338-3610<strong><br />
-            </strong></p>
+            <p>We offer 18 month financing with no interest through Synchrony Bank. Other options are available throughout the year. Call today for details and to see if you qualify. <a href="tel:2073383610">(207) 338-3610</a></p>
             <h3>3.Do you offer layaway?</h3>
             <p>Yes.</p>
             <h3>4. Do you have gift certificates?</h3>
             <p>Yes.</p>
             <h3>5. Do you accept COD orders?</h3>
-            <p>Yes.<br />
-        </div></p>
+            <p>Yes.</p>
+        </div>
 
         <div class="column-unbreakable">
+            <h2>Website Stuff</h2>
+            <h3>1. Who took those photos?</h3>
+            <p>Two of our employees, Laurie and Gardiner.</p>
+            <h3>2. Who made your website?</h3>
+            <p>One of our employees wrote everything from scratch (except for the promotions page which is based on Wordpress). You can check out his <a href="http://github.com/heavyelement/">github</a> or see his <a href="http://heavyelement.github.io">portfolio.</a></p>
+            <h3>3. I found a bug, typo, or error. What should I do?</h3>
+            <p>You can email gardinermoody (at) perrysfurniture (dot) com.</p>
+            <p>Or, if you know what you're doing, you can submit an issue on our <a href="http://github.com/heavyelement/perrys-web/issues">GitHub page</a>.</p>
+            <h3>4. Where can I find your Television Ads?</h3>
+            <p>We have a <a href="http://youtube.com/perryshome">YouTube channel</a> where we keep them archived, as well as other videos.</p>
 
-            <h2>Website FAQs and Policies</h2>
-            <h3>1. Why are there no prices shown on your web site?</h3>
-            <p>With the constantly changing cost of production and delivery, the cost to us is always fluctuating. It is a high priority for us to offer our customers the best possible value so we do not list our prices on our website. If you have a question regarding the price of an item, feel free to call us at (207) 338-3610 so we can give you the best current pricing. We do offer discount furniture, clearance items, and floor samples with prices listed on our Promotions page.</p>
-            <h3>2. Do you have a mailing list?</h3>
-            <p>We send out newsletters, updates, and store event information to our customers via email. If you would like to sign up, please enter your email in the box on the right side of the page. If at any time you wish to be removed from our mailing list, simply click the unsubscribe option from an email we have sent.</p>
-            <h3>3. Do you share my information with anyone?</h3>
-            <p>We highly value and protect your privacy as our customer. We do not and will never share our customer information with anyone else.</p>
-            <h3>4. Do you have an email newsletter?</h3>
-            <p>We do send out emails telling our customers about upcoming events and current happenings in our store. We also maintain a blog where you can check to see what’s new in the store and other fun projects our staff is working on. If you are active via social media, we encourage you to like us on Facebook and follow us on Twitter, Pinterest, Youtube, and Houzz. We are always looking for new and exciting ways to reach out and interact with our customers.</p>
-            <h3>5. Can I view your TV commercials on the web site?</h3>
-            <p>We maintain a youtube channel where you can view our TV commercials as well as other videos that welcome you into the world of Perry’s Home Furnishings &#038; Design. You can connect to this channel via our youtube channel button at the top of our page.
-        </div></p>
-        
+        </div>
+
         <div class="column-unbreakable">
-
-            <h2>Furniture Delivery FAQs and Policies</h2>
-            <h3>1. I have a question about the delivery status of my furniture. How can I get help?</h3>
-            <p>Call our friendly and knowledgeable staff at the store anytime during open hours at (207) 338-3610. We can also be reached via email or through our Contact Us page.</p>
-            <h3>2. What if, after delivery, I notice damage on my merchandise?</h3>
-            <p>We take great pride in our top notch delivery and inspection procedures. Upon delivery, if you have any concerns, please call us at (207) 338-3610.</p>
-            <h3>3. What are your delivery charges?</h3>
+            <h2>Furniture Delivery Policies</h2>
+            <h3>1. What are your delivery charges?</h3>
             <p>All deliveries within Waldo County are free. Any deliveries outside of Waldo County will be determined by mileage.</p>
-            <h3>4. Can I pick up my furniture myself and avoid delivery charges?</h3>
-            <p>You are welcome to pick up any merchandise yourself. All merchandise must be secured before leaving the premises and the merchandise becomes your responsibility. You will also be responsible for any damage received in transporting the merchandise.</p>
-            <h3>5. Do you deliver to my area?</h3>
+            <h3>2. Do you deliver to my area?</h3>
             <p>We deliver to the entire state of Maine. We offer free delivery within Waldo County.</p>
-            <h3>6. What additional services do my shipping/delivery charges include?</h3>
+            <h3>3. What additional services do my shipping/delivery charges include?</h3>
             <p>Set up of new furniture and removal of old furniture are included with delivery. Gently used merchandise may be able to be donated to a charity, such as New Hope for Women.</p>
-            <h3>7. Do I have to wait at home all day for delivery?</h3>
-            <p>No. Our deliveries are set up in 1-2 hour increments Monday through Friday. Saturdays are available with special arrangements.</p>
-            <h3>8. How do I postpone a delivery?</h3>
-            <p>If, for some reason, you need to reschedule your delivery call us at (207) 338-3610 and we will work with you to set up a more convenient delivery time.<br />
-        </div></p>
-
-        <div class="column-unbreakable">
-            <h2>Furniture Care &amp; Maintenance FAQs and Policies</h2>
-            <h3>Whom do I contact with concerns or questions regarding how to clean and maintain my furniture?</h3>
-            <p>If you have any concerns or questions regarding how to clean and maintain your furniture, call us at (207) 338-3610. Our friendly and knowledgeable sales team is more than happy to help you.<br />
-        </div></p>
-
-        <p><div class="column-unbreakable">
-            <h2>Deciding on Your New Furniture FAQs and Policies</strong></h2>
-            <h3>1. Do you take custom orders?</h3>
-            <p>Yes. Call us to speak to our experienced staff. We will happily help you create the furniture piece or pieces you want for your own personalized look.</p>
-            <h3>2. Can I supply my own fabric when ordering upholstered furniture?</h3>
-            <p>Yes, most of the vendors we work with are able to use customer’s own fabric when upholstering a piece of furniture.</p>
-            <h3>3. How can I see or order fabric samples? Wood finish samples?</h3>
-            <p>Come in to the store to see the plethora of fabric and wood finish samples we have. You can take a few samples home with you to make sure they work for your style.</p>
-            <h3>4. What styles of furniture does Perry’s Home Furnishings &#038; Design carry?</h3>
-            <p>We aim to provide a wide range of styles and selection for our eclectic clientele. The most popular styles we carry include: Transitional, Coastal, Mission, Classic, Contemporary, Traditional, Rustic and Retro.</p>
-            <h3>5. What should I consider before buying?</h3>
-            <p>If you are only just beginning to think about redesigning a room, we encourage you to come in to our furniture showroom and take a walk around. We offer displays of room settings that allow you to visualize how your room will look, including various colors of painted walls that provide inspiration and ideas for your home. However, if you are ready to make more concrete decisions about designing your space we recommend the following steps:</p>
-            <ul>
-                <li>take measurements</li>
-                <li>sketch and/or take photos of the room</li>
-                <li>look for inspiration</li>
-                <li>set a budget</li>
-                <li>and then come shopping.</li>
-            </ul>
-            <p>If any of these steps becoming overwhelming or just plain no fun, call us and we will gladly help guide you the rest of the way in your home interior design process. We offer free home design consultations for all our customers.</p>
-            <h3>6. I&#8217;ve been doing a lot of furniture shopping, and have found that most stores look alike and carry the same merchandise. How is your store different?</h3>
-            <p>At Perry’s Home Furnishings &#038; Design, we pride ourselves on our outstanding customer service. We truly listen to our customers. With decades of experience, we are able to help you understand exactly what you’re looking for even when you are having trouble articulating it yourself. We strive to provide customer service that is above and beyond any you can find in another store. We offer a multitude of customization furniture options to create the exact look you want. With nearly endless options and unparalleled customer service, we can help you coordinate your entire home décor look. Visit our furniture showroom for a furniture shopping experience that is easy and beautiful. You won’t find the same experience in any other furniture store that you will have at Perry’s Home Furnishings &#038; Design.<br />
-        </div></p>
+            <h3>4. Do I have to wait at home all day for delivery?</h3>
+            <p>No. Our deliveries are set up in 1-2 hour increments Monday through Friday. Saturdays are available with <em>special</em> arrangements.</p>
         </div>
     </div>
 </div>
-		
+
 <?php include '../template-parts/lower.php'; ?>
