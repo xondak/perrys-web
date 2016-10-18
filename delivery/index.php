255d93b39d2a81f69a811782d125a3cbe4af1e1b
heavyelement <gardiner.moody@gmail.com>
2016-10-18 12:01:17 -0400

    Created the 'delivery' page, but there's nothing here yet.

---
+++ delivery/index.php
@@ -0,0 +1,31 @@
+<?php $LocName="Delivery";
+	  $location="delivery";
+	  $noContent="no-content";
+	  $splashContent="";
+	  include '../template-parts/upper.php';
+?>
+<div id="content" class="wrapper">
+	<div class="container">
+		<h1>Delivery Services</h1>
+		<div class="center">
+			<h2>Delivery Throughout Maine</h2>
+			<p>Our delivery services are free within Waldo County, and fees are minimal throughout the rest of the state.
+			</p>
+		</div>
+		<div class="center">
+			<h2>Free Removal</h2>
+			<p>While we're delivering your new furnishings, we offer complementary removal of unwanted furniture. Sofas, loveseats, occasional tables, beds, 
+			mattresses, and more. Gently used items are then donated to local charities.
+			</p>
+		</div>
+		<div class="center">
+			<h2>Donation</h2>
+			<p>We work with three local charities to get used furniture into the hands of those who need it. Our commitment to our community includes a strong 
+			relationship with <a href="http://www.newhopeforwomen.org/">New Hope for Women</a>, <a href="http://homehelphope.org/">Knox County Homeless Coalition</a>, 
+			and <a href="http://www.midcoasthabitat.org/">Midcoast Habitat Restore</a>. Gently used items are continually donated, providing comfort in transitional 
+			housing situations.
+			</p>
+		</div>
+	</div>
+</div>
+<?php include '../template-parts/lower.php'; ?>
