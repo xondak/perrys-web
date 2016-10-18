095113fc7504d61f2475584b3de1391a1a818772
heavyelement <gardiner.moody@gmail.com>
2016-10-18 14:14:45 -0400

    Added OpenGraph and removed redundent entries.

--- template-parts/meta.php
+++ template-parts/meta.php
@@ -2,9 +2,12 @@
 		<html lang="en-US">
 		<link rel="stylesheet" type="text/css"  href="/demo/style.css">
 		<meta charset="UTF-8">
-		<meta charset="UTF-8">
-		<meta name="description" content="An ever-changing assortment of fine home furnishings.">
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<meta http-equiv="Cache-Control" content="max-age=1900"/>
-		<meta name="keywords" content="Perry&#039;s Home Furnishings &amp; Design,Furniture Store Maine,Furniture Belfast Maine,Home accessories,interior design services Maine,real estate staging services,Magnolia Home,Joanna Gaines Collection,Magnolia Home Furniture,Joanna Gaines,Chip and Joanna Gaines,Flexsteel,La-z-boy,Lazboy,Best Home Furnishings,Laura Ashley,Tempurpedic,recliners,mattresses,occasional tables,dining room table,dining room set">
 		<meta name="description" content="Maine's finest home furnishings and accessories. Come experience the difference in Belfast, Maine!">
+
+		<meta property="og:title" content="We are Perry's Home Furnishings & Design"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:image" content="http://perrysfurniture.com/primary.jpg"/>
+		<meta property="og:url" content="http://perryshomefurnishingsanddesign.com"/>
+		<meta property="og:description" content="Have you ever wanted to mix up your living room? Is there another room you have a vision for? We not only provide the highest quality assortment of fine home furnishings, but we also have the designers (and friends) who'll help you realize your dream home."/>
+		<meta property="fb:admins" content="173679905998072"/>
