250cba440f9f7eac2c28e025b872bc4519c592e7
heavyelement <gardiner.moody@gmail.com>
2017-08-15 11:41:05 -0400

    Created the new-arrivals page, gave it a markdown rendering engine powered by Parsedown.php, and added the ability to show single pages. Will add paginated history with next commit.

--- new-arrivals/index.php
+++ new-arrivals/index.php
@@ -1,18 +1,57 @@
-<?php $LocName="New Arrivals";
-	  $location="new";
-	  $splashContent="<img src='../img/vendors/logos/mag.png'>";
-	  include '../template-parts/upper.php';
+<?php
+	include 'Parsedown.php';
+	$Parsedown = new Parsedown(); // Invokes Parsedown -- Moved from inside the foreach loop
+	$dir = 'posts/'; // $dir defines the directory to search for the .md Markdown files
+
+	if ( htmlspecialchars($_GET["id"]) == 0 ) {
+		renderFive($dir, $Parsedown);
+	}
+	else {
+		renderSingle($dir, $Parsedown);
+	}
+
+	function startPage($title, $loc, $splash, $noc){ // Begins page rendering
+		$LocName=$title; // Sets page title
+		$location=$loc; // Sets location (for nav bar)
+		$splashContent=$splash; // Sets splash content
+		$noContent=$noc; // Defines if the page lacks a splash
+		include '../template-parts/upper.php'; // Begins rendering page
+	}
+
+	function renderFive($dir, $Parsedown){
+		$indir = array_filter(scandir($dir), function($item) { // This removes the . and .. directories from the array
+			return $item[0] !== '.';
+		});
+		$reversed = array_reverse($indir); // Reverses the display order
+		$posts = array_slice($reversed,  0, 3); // This grabs the first three entries in the array
+
+		startPage("New Arrivals", "new", "<h1>New Arrivals</h1>", null); // Passes relevant info to the startPage function.
+
+		print("<article class='container post'>"); // Generates the article wrapper
+
+		foreach ($posts as &$value) {
+			print("<div class='body'><div class='center new-posts'>"); // Creates individual post wrappers
+			$post = $dir . $value; // Appends 'posts/' to each file
+			echo is_readable($post) ? $Parsedown->text(file_get_contents($post)) : "<h1>Can't find ".htmlspecialchars($post)."</h1>"; // Imports .md files and pipes it through Parsedown, fails graciously
+			echo "<div class='permalink-wrapper'><a href='?id=" . substr($value, 0, -3) . "'><div class='permalink'></div></a></div>"; // Strips the .md from permalink and adds it to the end of each page
+			print("</div></div>"); // Closes each post's wrapper
+		}
+		
+		print("</article>"); // Closes main wrapper
+	}
+	
+	function renderSingle($dir, $Parsedown){
+		$post = $dir . htmlspecialchars($_GET["id"]) . ".md"; // Appends 'posts/' to each file
+		if (is_readable($post) == true) {
+			startPage(htmlspecialchars($_GET["id"]), "new", " ", "no-content"); // Passes relevant info to the startPage function.
+			
+			print("<article class='container post'><div class='body'><div class='center new-posts'>"); // Creates individual post wrappers
+			echo $Parsedown->text(file_get_contents($post)); // Imports .md files and pipes it through Parsedown, fails graciously
+			print("</div></div></article>"); // Closes each post's wrapper
+		}
+		else{
+			include("../404/404.php");
+		}
+	}
+	include '../template-parts/lower.php'; // Includes the footer for the page
 ?>
-
-<div class="container">
-	<div  class="body">
-		<div id="auxiliary" class="center">
-			<h1>Welcome, Magnolia Home</h1>
-			<p>Perry's Home Furnishings and Design is excited to introduce you to Magnolia Home, the inspiring new line by Joanna Gaines. With a combination of family, aesthetic and practicality in mind, Joanna has thoughtfully designed each piece in the collection and instilled her well-loved style into every detail.</p>
-			<p>Our first capsule collection of the line is beginning to take shape in our showroom and we have more on the way! As the original authorized Magnolia Home source in Maine, we give you access to the entire collection. Our designers will work with you to blend Joanna's pieces together to create the perfect space for your family to call "home".</p>
-			<p>Be the first to experience Magnolia Home by Joanna Gaines. It's as close as midcoast Maine. We look forward to sharing it all with you!</p>
-		</div>
-	</div>
-</div>
-
-<?php include '../template-parts/lower.php'; ?>
